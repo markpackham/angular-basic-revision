49220267c3f62850158c81251177d625d99d5654
mark <markpackham1@gmail.com>
2021-10-23 16:54:45 +0100

    send message between child, parent and main app
    https://youtu.be/AAu8bjj6-UI?t=984

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule
